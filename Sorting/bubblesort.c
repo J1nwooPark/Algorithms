ef8ea7d0c0d79c12e1a8e2abc2afd85ca23cca79
Jinwoo Park <49577959+J1nwooPark@users.noreply.github.com>
2019-12-28 15:17:01 +0900

    Create bubblesort.c

---
+++ Sorting/bubblesort.c
@@ -0,0 +1,18 @@
+void BubbleSort(int arr[], int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		for (int j = 0; j < size - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
