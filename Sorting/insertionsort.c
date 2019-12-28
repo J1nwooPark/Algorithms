f0bedf5b2b80613f12c2342f8e2cf9eff1866471
Jinwoo Park <49577959+J1nwooPark@users.noreply.github.com>
2019-12-28 15:17:51 +0900

    Create insertionsort.c

---
+++ Sorting/insertionsort.c
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void InsertionSort(int arr[], int size)
+{
+	for (int i = 1; i < size; i++)
+	{
+		int cur = arr[i];
+		int j = i - 1;
+		while (j >= 0 && cur < arr[j])
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = cur;
+	}
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
