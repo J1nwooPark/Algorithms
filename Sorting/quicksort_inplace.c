35bd8d564c0d8c73b3c27c16b5d1b4b20bba9d97
Jinwoo Park <49577959+J1nwooPark@users.noreply.github.com>
2019-12-30 03:15:14 +0900

    Create quicksort_inplace.c

---
+++ Sorting/quicksort_inplace.c
@@ -0,0 +1,22 @@
+void swap(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+void QuickSort_inplace(int arr[], int start, int end)
+{
+	if (start >= end) return;
+	int pivot = arr[end];
+	int left = start, right = end - 1;
+	while (left <= right)
+	{
+		while (left <= right && arr[left] <= pivot) left++;
+		while (right >= left && arr[right] >= pivot) right--;
+		if (left < right)
+			swap(&arr[left], &arr[right]);
+	}
+	swap(&arr[left], &arr[end]);
+	QuickSort_inplace(arr, start, left - 1);
+	QuickSort_inplace(arr, left + 1, end);
+}
